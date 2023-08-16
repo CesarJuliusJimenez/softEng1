de0aadd31ef665d25c3f7639feac71dc49b0b451
CesarJuliusJimenez <116608904+CesarJuliusJimenez@users.noreply.github.com>
2023-08-16 13:46:45 +0800

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+  public static void main (Strings[] args) {
+    System.out.println("Hello, Software Engineering Course!");
+    System.out.println("My name is JIMENEZ, Cesar Julius D.");
+  }
+}
