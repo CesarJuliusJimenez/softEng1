032a6c7959328f292889156b8212c49932d15a98
Cesar Jimenez <jimenezcesarjuliusd.@gmail.com>
2023-08-25 21:23:24 +0800

    adding some changes to Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
   public static void main (Strings[] args) {
     System.out.println("Hello, Software Engineering Course!");
     System.out.println("My name is JIMENEZ, Cesar Julius D.");
+    System.out.println("That is why developers are highly-paid!");
   }
 }
